5979c478a33d8cb87395a78dd4758f2770f97a0c
Renato <pomborenato@gmail.com>
2021-01-25 16:57:11 -0300

    Codigo Primeira aula

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,53 @@
 import styled from 'styled-components'
+import db from '../db.json'
+import Widget from '../src/components/Widget'
+import Footer from '../src/components/Footer'
+import GitHubCorner from '../src/components/GitHubCorner'
+import QuizBackground from '../src/components/QuizBackground'
 
-const Title = styled.h1`
-  font-size: 50px;
-  color: ${({ theme }) => theme.colors.primary};
-`
+export const QuizContainer = styled.div`
+    width: 100%;
+    max-width: 350px;
+    padding-top: 45px;
+    margin: auto 10%;
+    @media screen and (max-width:500px){
+      margin:auto;
+      padding: 15px;
+    }
+`;
+
+const BackgroundImage = styled.div`
+    background-image: url(${db.bg});
+    flex: 1;
+    background-size: cover;
+    background-position: center;
+`;
 
 export default function Home() {
-  return <Title>My page</Title>
+  console.log(db.bg)
+  return (
+    <QuizBackground backgroundImage={db.bg}>
+      <QuizContainer>
+       lskdnlsdkfnsjkdfn
+       <Widget>
+       <Widget.Header>
+              <h1>Quis do Chavinho</h1>
+            </Widget.Header>
+          <Widget.Content>
+         
+            <p>sdfsdf</p>
+          </Widget.Content>
+       </Widget>
+
+       <Widget>
+          <Widget.Content>
+            <h1>Quis do Chavinho</h1>
+            <p>sdfsdf</p>
+          </Widget.Content>
+       </Widget>
+        <Footer/>
+      </QuizContainer>
+      <GitHubCorner projectUrl="" />
+    </QuizBackground>
+  )
 }
