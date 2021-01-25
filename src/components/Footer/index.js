d10f149298934bb094b5c681e4e05ad62991f053
Renato <pomborenato@gmail.com>
2021-01-25 17:20:17 -0300

    correção

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -33,12 +33,12 @@ export default function Footer(props) {
         <img src="https://www.alura.com.br/assets/img/alura-logo-white.1570550707.svg" alt="Logo Alura" />
       </a>
       <p>
-        Orgulhosamente criado durante
+        Orgulhosamente criado por Renato Romanhol durante
         {' '}
         a
         {' '}
         <a href="https://www.alura.com.br/">
-          <span>Imersão React da Alura</span>
+          <span>Imersão React+NextJS da Alura</span>
         </a>
       </p>
     </FooterWrapper>
