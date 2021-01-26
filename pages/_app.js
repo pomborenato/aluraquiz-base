b340c73a41251ebb008f127c9e8ddab27e228298
Renato <pomborenato@gmail.com>
2021-01-26 09:00:44 -0300

    adicionando eslint e tags ogg na head

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,8 @@
-import { createGlobalStyle, ThemeProvider } from 'styled-components'
+/* eslint-disable react/prop-types */
+/* eslint-disable react/jsx-props-no-spreading */
+import React from 'react';
+import { createGlobalStyle, ThemeProvider } from 'styled-components';
+import Head from 'next/head';
 import db from '../db.json';
 
 const GlobalStyle = createGlobalStyle`
@@ -23,18 +27,36 @@ const GlobalStyle = createGlobalStyle`
     display: flex;
     flex-direction: column;
   }
-`
+`;
 
-const theme = db.theme;
+const { theme } = db;
 
 export default function App({ Component, pageProps }) {
   return (
     <>
-     
+      <Head>
+        <title>QUIZ DO CHAVINHO</title>
+        <meta name="title" content="Meta Tags — Preview, Edit and Generate" />
+        <meta name="description" content="Imersão Alura Quiz do Chavinho" />
+
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content="https://aluraquiz-base.pomborenato.vercel.app/" />
+        <meta property="og:title" content="QUIZ DO CHAVINHO" />
+        <meta property="og:description" content="Imersão Alura Quiz do Chavinho" />
+        <meta property="og:image" content="https://img.elo7.com.br/product/zoom/1EDA6CE/painel-lona-turma-do-chaves-desenho-animado-painel-decorativo-chaves.jpg" />
+
+        <meta property="twitter:card" content="summary_large_image" />
+        <meta property="twitter:url" content="https://aluraquiz-base.pomborenato.vercel.app/" />
+        <meta property="twitter:title" content="QUIZ DO CHAVINHO" />
+        <meta property="twitter:description" content="Imersão Alura Quiz do Chavinho" />
+        <meta property="twitter:image" content="https://img.elo7.com.br/product/zoom/1EDA6CE/painel-lona-turma-do-chaves-desenho-animado-painel-decorativo-chaves.jpg" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" />
+        <link href="https://fonts.googleapis.com/css2?family=Lato:ital,wght@0,100;0,300;0,400;0,700;0,900;1,100;1,300;1,400;1,700;1,900&display=swap" rel="stylesheet" />
+      </Head>
       <ThemeProvider theme={theme}>
-      <GlobalStyle />
+        <GlobalStyle />
         <Component {...pageProps} />
       </ThemeProvider>
     </>
-  )
+  );
 }
--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,10 @@
-import styled from 'styled-components'
-import db from '../db.json'
-import Widget from '../src/components/Widget'
-import Footer from '../src/components/Footer'
-import GitHubCorner from '../src/components/GitHubCorner'
-import QuizBackground from '../src/components/QuizBackground'
+import React from 'react';
+import styled from 'styled-components';
+import db from '../db.json';
+import Widget from '../src/components/Widget';
+import Footer from '../src/components/Footer';
+import GitHubCorner from '../src/components/GitHubCorner';
+import QuizBackground from '../src/components/QuizBackground';
 
 export const QuizContainer = styled.div`
     width: 100%;
@@ -16,38 +17,28 @@ export const QuizContainer = styled.div`
     }
 `;
 
-const BackgroundImage = styled.div`
-    background-image: url(${db.bg});
-    flex: 1;
-    background-size: cover;
-    background-position: center;
-`;
-
 export default function Home() {
-  console.log(db.bg)
   return (
     <QuizBackground backgroundImage={db.bg}>
       <QuizContainer>
-       
-       <Widget>
-       <Widget.Header>
-              <h1>Quiz do Chavinho</h1>
-            </Widget.Header>
+        <Widget>
+          <Widget.Header>
+            <h1>Quiz do Chavinho</h1>
+          </Widget.Header>
           <Widget.Content>
-         
             <p>sdfsdf</p>
           </Widget.Content>
-       </Widget>
+        </Widget>
 
-       <Widget>
+        <Widget>
           <Widget.Content>
             <h1>Quiz do Chavinho</h1>
             <p>sdfsdf</p>
           </Widget.Content>
-       </Widget>
-        <Footer/>
+        </Widget>
+        <Footer />
       </QuizContainer>
       <GitHubCorner projectUrl="https://github.com/pomborenato/aluraquiz-base" />
     </QuizBackground>
-  )
+  );
 }
